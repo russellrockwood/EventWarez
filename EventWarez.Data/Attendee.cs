ac514005e91d251793c927b4cd249b85a1849074
Ryan <Dibzthedibz@gmail.com>
2021-06-07 18:31:21 -0700

    Ticket and Attendee Objects Added

---
+++ EventWarez.Data/Attendee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventWarez.Data
+{
+    public class Attendee
+    {
+        [Key]
+        public int AttId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string AccessLevel { get; set; }
+        public virtual List<Ticket> Tickets { get; set; }
+    }
+}
---
+++ EventWarez.Data/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventWarez.Data
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Attendee))]
+        public int AttId { get; set; }
+        public virtual Attendee Attendee { get; set; }
+        //[ForeignKey(nameof(Show))]
+        //public int ShowId { get; set; }
+        //public virtual Show Show { get; set; }
+        public int Price { get; set; }
+        public enum TicketType { General, VIP }
+        public TicketType TypeOfTicket { get; set; }
+    }
+}
