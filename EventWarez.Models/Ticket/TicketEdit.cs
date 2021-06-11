93311a391a9ca34bd7aaff67a6eb0d300259bb58
Russell Rockwood <russell.rockwood@gmail.com>
2021-06-11 09:13:54 -0500

    test2

---
+++ EventWarez.Models/Ticket/TicketEdit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventWarez.Models.Ticket
+{
+    public class TicketEdit
+    {
+        public int TicketId { get; set; }
+        public int AttId { get; set; }
+    }
+}
